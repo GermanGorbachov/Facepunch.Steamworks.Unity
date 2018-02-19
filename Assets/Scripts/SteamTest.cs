f60bc0f303dc87a80f59e4d0e0e876ac56c3e8b5
Garry Newman <garrynewman@users.noreply.github.com>
2018-02-19 11:03:37 +0000

    Updated natives, avatar tests

--- Assets/Scripts/SteamTest.cs
+++ Assets/Scripts/SteamTest.cs
@@ -9,37 +9,23 @@ public class SteamTest : MonoBehaviour
     private ServerList.Request serverRequest;
     private Leaderboard leaderBoard;
 
-    void Start ()
+    public SteamAvatar YourAvatar;
+    public SteamAvatar RandomAvatar;
+
+    IEnumerator Start()
 	{
         //
-        // Don't destroy this when loading new scenes
+        // Wait for client to start up
         //
-        DontDestroyOnLoad( gameObject );
+        while ( Client.Instance == null )
+            yield return null;
 
-        //
-        // Configure for Unity
-        // This is VERY important - call this before 
-        //
-	    Facepunch.Steamworks.Config.ForUnity( Application.platform.ToString() );
-
-        //
-        // Create the steam client using Rust's AppId
-        //
-        new Facepunch.Steamworks.Client( 252490 );
-
-        //
-        // Make sure we started up okay
-        //
-        if ( Client.Instance == null )
-        {
-            Debug.LogError( "Error starting Steam!" );
-            return;
-        }
+        YourAvatar.Fetch( Client.Instance.SteamId );
 
         //
         // Request a list of servers
         //
-	    {
+        {
 	        serverRequest = Client.Instance.ServerList.Internet();
 	    }
 
@@ -48,6 +34,17 @@ public class SteamTest : MonoBehaviour
         //
 	    {
             leaderBoard = Client.Instance.GetLeaderboard( "TestLeaderboard", Client.LeaderboardSortMethod.Ascending, Client.LeaderboardDisplayType.Numeric );
+        }
+
+        //
+        // Chang to random avatar every 20 seconds, to test callbacks
+        //
+        while ( true )
+        {
+            ulong steamid = 76561197960279927 + (ulong) UnityEngine.Random.Range( 0, 100000 );
+            RandomAvatar.Fetch( steamid );
+
+            yield return new WaitForSeconds( 20 );
         }
 	}
 
@@ -63,65 +60,68 @@ public class SteamTest : MonoBehaviour
     {
         GUILayout.BeginArea( new Rect( 16, 16, Screen.width - 32, Screen.height - 32 ) );
 
-        if ( Client.Instance != null )
+        if ( Client.Instance  == null )
         {
-            GUILayout.Label( "SteamId: " + Client.Instance.SteamId );
-            GUILayout.Label( "Username: " + Client.Instance.Username );
+            GUILayout.Label( "SteamClient not loaded.." );
+            GUILayout.EndArea();
+            return;
+        }
 
-            GUILayout.Label( "Friend Count: " + Client.Instance.Friends.AllFriends.Count() );
-            GUILayout.Label( "Online Friend Count: " + Client.Instance.Friends.AllFriends.Count( x => x.IsOnline ) );
 
-            if ( Client.Instance.Inventory.Definitions != null )
-                GUILayout.Label( "Item Definitions: " + Client.Instance.Inventory.Definitions.Length );
 
-            if ( Client.Instance.Inventory.Items != null )
-                GUILayout.Label( "Item Count: " + Client.Instance.Inventory.Items.Length );
+        GUILayout.Label( "SteamId: " + Client.Instance.SteamId );
+        GUILayout.Label( "Username: " + Client.Instance.Username );
 
-            if ( serverRequest != null )
+        GUILayout.Label( "Friend Count: " + Client.Instance.Friends.AllFriends.Count() );
+        GUILayout.Label( "Online Friend Count: " + Client.Instance.Friends.AllFriends.Count( x => x.IsOnline ) );
+
+        if ( Client.Instance.Inventory.Definitions != null )
+            GUILayout.Label( "Item Definitions: " + Client.Instance.Inventory.Definitions.Length );
+
+        if ( Client.Instance.Inventory.Items != null )
+            GUILayout.Label( "Item Count: " + Client.Instance.Inventory.Items.Length );
+
+        if ( serverRequest != null )
+        {
+            GUILayout.Label( "Server List: " + (serverRequest.Finished ? "Finished" : "Querying") );
+            GUILayout.Label( "Servers Responded: " + serverRequest.Responded.Count );
+            GUILayout.Label( "Servers Unresponsive: " + serverRequest.Unresponsive.Count );
+
+            if ( serverRequest.Responded.Count > 0 )
             {
-                GUILayout.Label( "Server List: " + (serverRequest.Finished ? "Finished" : "Querying") );
-                GUILayout.Label( "Servers Responded: " + serverRequest.Responded.Count );
-                GUILayout.Label( "Servers Unresponsive: " + serverRequest.Unresponsive.Count );
-
-                if ( serverRequest.Responded.Count > 0 )
-                {
-                    GUILayout.Label( "Last Server: " + serverRequest.Responded.Last().Name );
-                }
-            }
-
-            if ( leaderBoard != null )
-            {
-                GUILayout.Label( "leaderBoard.IsValid: " + leaderBoard.IsValid );
-                GUILayout.Label( "leaderBoard.IsError: " + leaderBoard.IsError );
-
-                if ( GUILayout.Button( "Refresh Leaderboard" ) )
-                {
-                    leaderBoard.FetchScores( Leaderboard.RequestType.Global, 0, 100 );
-
-                    leaderBoard.AddScore( true, 456, 1, 2, 3, 4, 5, 6 );
-                }
-
-                if ( leaderBoard.IsQuerying )
-                {
-                    GUILayout.Label( "QUERYING.." ); 
-                }
-                else if ( leaderBoard.Results != null )
-                {
-                    foreach ( var result in leaderBoard.Results )
-                    {
-                        GUILayout.Label( string.Format( "{0}. {1} ({2})", result.GlobalRank, result.Name, result.Score ) );
-                    }
-                }
-                else
-                {
-                    GUILayout.Label( "No Leaderboard results" );
-                }
+                GUILayout.Label( "Last Server: " + serverRequest.Responded.Last().Name );
             }
         }
-        else
+
+        if ( leaderBoard != null )
         {
-            GUILayout.Label( "SteamClient Failed. Make sure appropriate files are in root, and steam is running and signed in." );
+            GUILayout.Label( "leaderBoard.IsValid: " + leaderBoard.IsValid );
+            GUILayout.Label( "leaderBoard.IsError: " + leaderBoard.IsError );
+
+            if ( GUILayout.Button( "Refresh Leaderboard" ) )
+            {
+                leaderBoard.FetchScores( Leaderboard.RequestType.Global, 0, 100 );
+
+                leaderBoard.AddScore( true, 456, 1, 2, 3, 4, 5, 6 );
+            }
+
+            if ( leaderBoard.IsQuerying )
+            {
+                GUILayout.Label( "QUERYING.." ); 
+            }
+            else if ( leaderBoard.Results != null )
+            {
+                foreach ( var result in leaderBoard.Results )
+                {
+                    GUILayout.Label( string.Format( "{0}. {1} ({2})", result.GlobalRank, result.Name, result.Score ) );
+                }
+            }
+            else
+            {
+                GUILayout.Label( "No Leaderboard results" );
+            }
         }
+
 
         GUILayout.EndArea();
     }
